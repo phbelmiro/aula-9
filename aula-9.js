d2175890dc75301906a57a27157282fdf5e5b0ab
phbelmiro <142456085+phbelmiro@users.noreply.github.com>
2023-08-29 21:43:05 -0300

    Create aula-9.js

---
+++ aula-9.js
@@ -0,0 +1,109 @@
+// let nome = prompt("qual e seu nome?");
+// let cor = prompt("qual e sua cor favorita?");
+
+// console.log(`A cor favorito de ${nome}  é  ${cor}`);
+// console.log("A cor favorito de" + nome +  " é "  + cor);
+
+
+// const frase =  "OIIEEeeee!"
+
+// const fraseMinuscula = frase.toLowerCase()
+
+
+// const frase =  "OIIEEeeee!"
+
+// const fraseM = frase.toUpperCase()
+
+
+// const email = "mika@gmail.com "
+// console.log(email.trim())
+
+
+
+// const frase ="hoje comi cenoura, e adoro cenoura"
+
+// const novaFrase = frase.replaceAll("cenoura", "batata")
+ 
+// console.log(frase)
+// console.log(novaFrase)
+
+
+
+
+
+// let frase = prompt("Digite uma frase")
+
+// frase = frase.toUpperCase()
+// frase = frase.replaceAll("O", "i")
+
+// console.log(frase)
+// console.log(frase.length)
+
+
+// const  listaDeConpra = ["batata" , "alface", "queijo"]
+
+// const listaDeNumerosMega= [2, 13, 26, 35, 41, 60 ]
+
+// const meuArray = ["banana" , 15, true]
+
+
+// const listaDeConpra = ["abacate" , "banana" , "Tomates"]
+// const segundoItem = listaDeConpra[2]
+
+
+
+// const cachorro = ["Bulldog Francês" , "Shih Tzu" , "Rottweiler" , "Pug" ,  "Golden Retriever"]
+ 
+// const dog = Number(prompt("de 0 a 4"));
+
+// console.log(cachorro[dog]);
+
+
+
+// const pokemon = ["bulbasauro" , "aquirtle" , "charmander"]  
+
+// console.log(pokemon.length) //3
+
+// const serieBoas = ["The Big Bang Theory" , "Brooklyn Nini-Nine"]
+
+// serieBoas.includes("The Big Bang Theor") //true
+// serieBoas.includes("Game of Thrones") //false
+
+// const numeros = [1, 2, 3]
+
+// numeros.push(4)
+// console.log(numeros)
+
+// numeros.push(5, 6, 7)
+// console.log(numeros)
+
+
+// const meusPeixes = ["palhaço" , "mandarim" , "esturjão"]
+
+// meusPeixes.pop()
+// console.log(meusPeixes) //["palhaço" , "mandarim"]
+
+
+
+// const letras = ["A" , "B", "C" , "D" , "E" , "F" , "G" , "H"]
+
+
+// letras.splice(2, 1)
+
+// letras.splice(3, 2)
+
+
+// console.log(letras)
+
+
+const numeros = [1, 2, 3, 4, 5, 6]
+
+console.log(numeros.length)
+
+numeros.push(7)
+
+numeros.splice(3, 2)
+
+console.log(numeros.length)
+
+console.log(numeros)
